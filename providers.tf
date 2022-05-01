7eba5d3d4ce1ff158ec171701eb2babaf0ce9b97
Enes <66560757+devenes@users.noreply.github.com>
2022-05-01 04:56:41 +0300

    Create providers.tf

---
+++ providers.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "3.58.0"
+    }
+  }
+
+  # backend "s3" {
+  #   bucket = "terraform-env-bucket"
+  #   key    = "tf/terraform.tfstate"
+  #   region = "us-east-1"
+  # }
+}
+
+provider "aws" {
+  region = "us-east-1" # Define your AWS region you want to use for the resources to be created in
+}
