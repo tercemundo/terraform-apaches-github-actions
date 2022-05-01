ab727b3124d5220dd67a4d94bc270224e06d34fd
Enes <66560757+devenes@users.noreply.github.com>
2022-05-01 04:56:47 +0300

    Create sec.tf

---
+++ sec.tf
@@ -0,0 +1,37 @@
+resource "aws_security_group" "terraform_project_sg" {
+  name        = "terraform_project_sg"
+  description = "Terraform Project SG"
+  vpc_id      = data.aws_vpc.vpc.id
+
+  tags = {
+    Name = "Terraform Project SG"
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
